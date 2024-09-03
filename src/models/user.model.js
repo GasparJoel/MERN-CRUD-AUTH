5e0d1d6aaaeceeadacac739460e278712cd2f6f7
Joel Gt <0332201030@unjfsc.edu.pe>
2024-09-03 15:02:57 -0500

    Agregando el schema User

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,1 +1,23 @@
-    
+    import mongoose, {model, Schema}  from "mongoose";
+    const userSchema = Schema({
+            username  :{
+                type:String,
+                required:true,
+                trim:true
+            },
+            email :{
+                 type:String,
+                 required:true,
+                 trim:true,
+                 unique:true
+                 
+            },
+            password :{
+                type:String,
+                required:true,
+
+            }
+
+    })
+
+    export default mongoose.model('User',userSchema)
