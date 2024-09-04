7dc90352b7626b964e15171e8df449a9bb40f8f5
Joel Gt <0332201030@unjfsc.edu.pe>
2024-09-03 22:59:51 -0500

    Implementacion de rutas y controller regiSter y login

--- app.js
+++ app.js
@@ -1,5 +1,14 @@
-import express from 'express'   
+import express from 'express'  
+
+import  morgan  from "morgan";
+import authRoutes from "./src/routes/auth.routes.js";
 
 const app = express();
+app.use(morgan('dev'))
+
+//agregamos las rutas
+app.use(authRoutes)
+
+
 
  export default app ;
