c95c9d3a06c697b5e5ff67464c33c65ff77f98f5
Joel Gt <0332201030@unjfsc.edu.pe>
2024-09-04 22:02:15 -0500

    Implementacion de hash para poder incriptar el password con bcryptjs

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,22 +1,36 @@
 
 import  User  from "../models/user.model.js";
+import bcrypt from "bcryptjs";
 
 export const register = async(req,res)=>{
     const {email,password,username} = req.body
 
    try {
-    const newUser =  new User({
-        username,
-        email,
-        password
-       }) 
-       
-       await newUser.save()
+    // Hash de la contraseña
+    const passwordHash = await bcrypt.hash(password, 10);
+
+    // Creación del nuevo usuario
+    const newUser = new User({
+      username,
+      email,
+      password: passwordHash,
+    });
+
+    // Guardar usuario en la base de datos
+    const userSaved = await newUser.save();
+
+    // Respuesta exitosa
+    return res.status(201).json({
+      id: userSaved._id,
+      username: userSaved.username,
+      email: userSaved.email,
+    });
    } catch (error) {
     console.log(error)
    }
-    res.send('Registrado')
+   
 }
+
 export const login = (req,res)=>{
     res.send('login')
 }
