efff3ab6b92dd54c2bbc989bedd9cf241cb3714f
Joel Gt <0332201030@unjfsc.edu.pe>
2024-09-11 10:38:06 -0500

    Implementar el metodo y la ruta logout

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -75,3 +75,10 @@ export const login = async(req,res)=>{
  }
  
 }
+
+export const logout = async(req,res)=>{
+  res.cookie('token',"",{
+    expires:new Date(0) // Expira la cookie de inmediato
+  })
+  return res.status(200).send('Logout successfull')
+}
