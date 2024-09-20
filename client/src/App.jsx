f0f2f8e4db95fd840e400d11f3268115dc97c18b
Joel Gt <0332201030@unjfsc.edu.pe>
2024-09-20 14:40:52 -0500

    Agregarmos LA IMPORTACION DE Tailwind y creamos las rutas  con react-route-dom

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+import { BrowserRouter,Routes,Route } from "react-router-dom";
 
+export const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <BrowserRouter >
+        <Routes>
+            <Route path="/" element={<h1>Home Page</h1>}/>
+            <Route path="/login" element={<h1>login</h1>}/>
+            <Route path="/register" element={<h1>register</h1>}/>
+            <Route path="/tasks" element={<h1>tasks</h1>}/>
+            <Route path="/add-task" element={<h1>add task</h1>}/>
+            <Route path="/tasks/:id" element={<h1>task looking</h1>}/>
+            <Route path="/profile" element={<h1>profile</h1>}/>
+            
+        </Routes>
+    </BrowserRouter>
   )
 }
-
-export default App
