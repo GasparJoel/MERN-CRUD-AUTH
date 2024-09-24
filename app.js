4ce4067c36d583f06c7753028573963e62321187
Joel Gt <0332201030@unjfsc.edu.pe>
2024-09-23 23:22:56 -0500

    Agregamos el cors que permite comunicar la API con el frontend que son de diferentes dominios y agregue codigo en register y consumir la API

--- app.js
+++ app.js
@@ -4,8 +4,13 @@ import  morgan  from "morgan";
 import authRoutes from "./src/routes/auth.routes.js";
 import taskRoutes from "./src/routes/task.routes.js";
 import  cookieParser  from "cookie-parser";
+import cors from 'cors';
 
 const app = express();
+
+app.use(cors({
+    origin:'http://localhost:5173',
+}))
 app.use(express.json())
 app.use(morgan('dev'))
 app.use(cookieParser())
--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,19 +1,21 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { LoginPage } from "./pages/LoginPage";
+import { RegistPage } from "./pages/RegistPage";
 
-import { BrowserRouter,Routes,Route } from "react-router-dom";
+
 
 export const App = () => {
   return (
-    <BrowserRouter >
-        <Routes>
-            <Route path="/" element={<h1>Home Page</h1>}/>
-            <Route path="/login" element={<h1>login</h1>}/>
-            <Route path="/register" element={<h1>register</h1>}/>
-            <Route path="/tasks" element={<h1>tasks</h1>}/>
-            <Route path="/add-task" element={<h1>add task</h1>}/>
-            <Route path="/tasks/:id" element={<h1>task looking</h1>}/>
-            <Route path="/profile" element={<h1>profile</h1>}/>
-            
-        </Routes>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<h1>Home Page</h1>} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={ <RegistPage/> } />  
+        <Route path="/tasks" element={<h1>Tasks Page</h1>} />
+        <Route path="/add-task" element={<h1>New Task</h1>} />
+        <Route path="/tasks/:id" element={<h1>Update Task</h1>} />
+        <Route path="/profile" element={<h1>Profile</h1>} />
+      </Routes>
     </BrowserRouter>
-  )
-}
+  );
+};
