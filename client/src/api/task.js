a32ba25077fc3a75e3a8040507f343c7029d41d1
Joel Gt <0332201030@unjfsc.edu.pe>
2024-10-01 15:38:25 -0500

    Integré todos los metodos en task.js para la obtencion de los datos de tasks CREATE , GET , PUT, POST,DELETE

--- client/src/api/task.js
+++ client/src/api/task.js
@@ -1,4 +1,8 @@
 import axios from "./axios";
 
+export const getTasksRequest = ()=>axios.get('task')
 export const registerTaskRequest = (task)=> axios.post('/task',task);
+export const getTaskRequest = (id)=>axios.get(`/task/${id}`)
+export const updateTaskRequest = (task) =>axios.put(`/task/${task._id}`,task)
+export const deleteTaskRequest = (id) =>axios.delete(`/task/${id}`)
 
